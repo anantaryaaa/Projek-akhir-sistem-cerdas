5582bb8175763199b0b8918f13bd3124f3094012
Vynn <121730359+Vynn0@users.noreply.github.com>
2024-11-22 10:03:44 +0700

    Update virtualPainter.py
    Virtual Painter

--- virtualPainter.py
+++ virtualPainter.py
@@ -1,109 +1,62 @@
 import cv2
 import numpy as np
-import time
-import os
-import handTrackingModule as htm
+from handTrackingModule import handDetector
 
-#######################
-brushThickness = 25
-eraserThickness = 100
-########################
+def main():
+    # Initialize camera
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    
+    # Create canvas for drawing
+    canvas = np.zeros((480, 640, 3), np.uint8)
+    
+    # Previous point coordinates
+    px, py = 0, 0
 
-folderPath = "C://IT//artificial-intelligent//mk-sistem-cerdas//project-2//Header"
-myList = os.listdir(folderPath)
-print(myList)
-overlayList = []
-for imPath in myList:
-    image = cv2.imread(f'{folderPath}/{imPath}')
-    overlayList.append(image)
-print(len(overlayList))
-header = overlayList[0]
-drawColor = (255, 0, 255)
+    # Create fullscreen window
+    window_name = "Drawing"
+    cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    
+    while True:
+        # Get camera frame
+        success, img = cap.read()
+        img = cv2.flip(img, 1)  # Mirror image
+        
+        # Find hand landmarks
+        img = detector.findHands(img)
+        lm_list = detector.findPosition(img, draw=False)
+        
+        if len(lm_list) > 0:  # If hand detected
+            # Get index finger tip coordinates
+            x, y = lm_list[8][1], lm_list[8][2]
+            
+            # Draw line if not first point
+            if px == 0 and py == 0:
+                px, py = x, y
+            else:
+                # Draw on both canvas and image
+                cv2.line(canvas, (px, py), (x, y), (0, 0, 255), 15)
+                cv2.line(img, (px, py), (x, y), (0, 0, 255), 15)
+            
+            # Update previous point
+            px, py = x, y
+        else:
+            # Reset previous point when hand not detected
+            px, py = 0, 0
+        
+        # Combine canvas and camera feed
+        img = cv2.add(img, canvas)
+        
+        # Show result
+        cv2.imshow(window_name, img)
+        
+        # Quit on 'q' press
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
 
-cap = cv2.VideoCapture(1)
-cap.set(3, 1280)
-cap.set(4, 720)
-
-detector = htm.handDetector(detectionCon=0.65, maxHands=1)
-xp, yp = 0, 0
-imgCanvas = np.zeros((720, 1280, 3), np.uint8)
-
-while True:
-
-    # 1. Import image
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
-
-    # 2. Find Hand Landmarks
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-
-    if len(lmList) != 0:
-
-        # print(lmList)
-
-        # Tip of index and middle fingers
-        x1, y1 = lmList[8][1:]
-        x2, y2 = lmList[12][1:]
-
-        # 3. Check which fingers are up
-        fingers = detector.fingersUp()
-        # print(fingers)
-
-        # 4. If Selection Mode – Two finger are up
-        if fingers[1] and fingers[2]:
-            # xp, yp = 0, 0
-            print("Selection Mode")
-
-            # Checking for the click
-            if y1 < 125:
-                if 250 < x1 < 450:
-                    header = overlayList[0]
-                    drawColor = (255, 0, 255)
-                elif 550 < x1 < 750:
-                    header = overlayList[1]
-                    drawColor = (255, 0, 0)
-                elif 800 < x1 < 950:
-                    header = overlayList[2]
-                    drawColor = (0, 255, 0)
-                elif 1050 < x1 < 1200:
-                    header = overlayList[3]
-                    drawColor = (0, 0, 0)
-                cv2.rectangle(img, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
-
-        # 5. If Drawing Mode – Index finger is up
-        if fingers[1] and fingers[2] == False:
-            cv2.circle(img, (x1, y1), 15, drawColor, cv2.FILLED)
-            print("Drawing Mode")
-            if xp == 0 and yp == 0:
-                xp, yp = x1, y1
-
-            cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
-
-            # Eraser functionality (commented out for now)
-            # if drawColor == (0, 0, 0):
-            #     cv2.line(img, (xp, yp), (x1, y1), drawColor, eraserThickness)
-            #     cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
-            # else:
-            #     cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThickness)
-            #     cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
-
-            xp, yp = x1, y1
-
-        # Clear Canvas when all fingers are up (commented out for now)
-        # if all (x >= 1 for x in fingers):
-        #     imgCanvas = np.zeros((720
-
-        imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
-        _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
-        imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
-        img = cv2.bitwise_and(img,imgInv)
-        img = cv2.bitwise_or(img,imgCanvas)
-
-        # Setting the header image
-        img[0:125, 0:1280] = header
-        # img = cv2.addWeighted(img,0.5,imgCanvas,0.5,0)
-        cv2.imshow("Image", img)
-        cv2.imshow("Canvas", imgCanvas)
-        cv2.imshow("Inv", imgInv)
-        cv2.waitKey(1)
+if __name__ == "__main__":
+    main()
